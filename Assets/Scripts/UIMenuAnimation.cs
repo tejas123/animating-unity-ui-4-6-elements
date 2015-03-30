180d54d82be34f23522f9bc6882c5c41ce869ad9
Bharat <bharatappguru@gmail.com>
2015-03-30 13:26:07 +0530

    Animating Unity UI 4.6 Elements Tutorial

---
+++ Assets/Scripts/UIMenuAnimation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIMenuAnimation : MonoBehaviour {
+	public GameObject panel;
+	Animator panelAnimation;
+	// Use this for initialization
+	void Start () {
+		panelAnimation = panel.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//this for settings button
+	public void OnClickSettings() {
+		Debug.Log("Settings Button Clicked");
+		panel.SetActive(true);
+		//Open Panel Animation
+		panelAnimation.SetInteger("isOpen", 1);
+
+	}
+
+	//this for panels close button
+	public void OnPanelCloseButton()
+	{
+		//close panel animation
+		panelAnimation.SetInteger("isOpen", 0);
+	}
+}
